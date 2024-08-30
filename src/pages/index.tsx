81aef2a0d2cebedafda3768acddd14d5437877e7
umershaikh123 <umershaikh217@gmail.com>
2024-08-30 20:36:07 +0500

    feat  : responsive button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import ColorTabs from "../Components/Common/Tabs"
 import { tabsProps } from "../constants/tabs"
 import Button from "@mui/material/Button"
 import AddIcon from "@mui/icons-material/Add"
-import SendIcon from "@mui/icons-material/Send"
+import Tippy from "@tippyjs/react"
+import "tippy.js/animations/scale.css"
+import "tippy.js/themes/translucent.css"
+
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col relative ">
@@ -15,25 +18,51 @@ const Home: NextPage = () => {
         <ColorTabs tabs={tabsProps} />
       </div>
 
-      <div className="absolute  right-20 top-10">
-        <Button
-          variant="contained"
-          endIcon={
+      <div className="absolute  right-20 top-12 hidden min-[850px]:flex">
+        <Tippy
+          placement="top"
+          animateFill={true}
+          animation={"scale"}
+          theme="custom"
+          content={`Create new Campaign`}
+        >
+          <Button
+            variant="contained"
+            endIcon={
+              <AddIcon
+                sx={{
+                  color: "var(--Bg)",
+                }}
+              />
+            }
+            sx={{
+              backgroundColor: "var(--primary)",
+              textTransform: "capitalize",
+
+              borderRadius: "6px",
+            }}
+          >
+            New Campaign
+          </Button>
+        </Tippy>
+      </div>
+
+      <div className="fixed bottom-8 right-5 flex min-[850px]:hidden">
+        <Tippy
+          placement="left"
+          animateFill={true}
+          animation={"scale"}
+          theme="custom"
+          content={`Create new Campaign`}
+        >
+          <button className=" rounded-full p-4 bg-[var(--primary)]">
             <AddIcon
               sx={{
-                color: "var(--Bg)",
+                color: "white",
               }}
             />
-          }
-          sx={{
-            backgroundColor: "var(--primary)",
-            textTransform: "capitalize",
-
-            borderRadius: "6px",
-          }}
-        >
-          New Campaign
-        </Button>
+          </button>
+        </Tippy>
       </div>
     </div>
   )
