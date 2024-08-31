6788ea83f6ed87c32bc1ec58580fb86bac3a1c50
umershaikh123 <umershaikh217@gmail.com>
2024-08-31 22:51:48 +0500

    images hover

--- src/Components/Common/ImagesRow.tsx
+++ src/Components/Common/ImagesRow.tsx
@@ -54,6 +54,7 @@ export default function ImageRowComponent() {
               alt={item.title}
               loading="lazy"
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              className=" hover:scale-110 transition-transform duration-300 ease-in-out"
             />
           </Box>
         ))}
