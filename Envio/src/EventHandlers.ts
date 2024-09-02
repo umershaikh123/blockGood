2e329516577f9a43c52fe2ddede07ee2baf0b7ee
umershaikh123 <umershaikh217@gmail.com>
2024-09-03 01:40:05 +0500

    envio

---
+++ Envio/src/EventHandlers.ts
@@ -0,0 +1,70 @@
+/*
+ * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+ */
+import {
+  Donationtracker,
+  Donationtracker_CampaignCreated,
+  Donationtracker_CampaignEnded,
+  Donationtracker_DonationReceived,
+  Donationtracker_ProofUploaded,
+  Donationtracker_WithdrawalMade,
+} from "generated";
+
+Donationtracker.CampaignCreated.handler(async ({ event, context }) => {
+  const entity: Donationtracker_CampaignCreated = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignId: event.params.campaignId,
+    creator: event.params.creator,
+  };
+
+  context.Donationtracker_CampaignCreated.set(entity);
+});
+
+
+Donationtracker.CampaignEnded.handler(async ({ event, context }) => {
+  const entity: Donationtracker_CampaignEnded = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignId: event.params.campaignId,
+    creator: event.params.creator,
+    refundedFee: event.params.refundedFee,
+  };
+
+  context.Donationtracker_CampaignEnded.set(entity);
+});
+
+
+Donationtracker.DonationReceived.handler(async ({ event, context }) => {
+  const entity: Donationtracker_DonationReceived = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignId: event.params.campaignId,
+    donor: event.params.donor,
+    amount: event.params.amount,
+  };
+
+  context.Donationtracker_DonationReceived.set(entity);
+});
+
+
+Donationtracker.ProofUploaded.handler(async ({ event, context }) => {
+  const entity: Donationtracker_ProofUploaded = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignId: event.params.campaignId,
+    withdrawalId: event.params.withdrawalId,
+    proofUrl: event.params.proofUrl,
+  };
+
+  context.Donationtracker_ProofUploaded.set(entity);
+});
+
+
+Donationtracker.WithdrawalMade.handler(async ({ event, context }) => {
+  const entity: Donationtracker_WithdrawalMade = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    campaignId: event.params.campaignId,
+    admin: event.params.admin,
+    amount: event.params.amount,
+  };
+
+  context.Donationtracker_WithdrawalMade.set(entity);
+});
+
--- src/Components/Common/Popup.tsx
+++ src/Components/Common/Popup.tsx
@@ -16,6 +16,7 @@ import "react-toastify/dist/ReactToastify.css"
 import { useAddRecentTransaction } from "@rainbow-me/rainbowkit"
 
 import donationTrackerAbi from "../../contracts/abis/donationTracker.json"
+import { donatationTracker_contractAddresses } from "../../constants/contracts"
 
 const DonationPopup = ({ handleClose }: { handleClose: () => void }) => {
   return (
@@ -127,7 +128,7 @@ export const RegisterIndividualPopup = ({
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
 
-    const contractAddress = "0xE8e1D0EC12BDc3eaA3510d898640e13ca48cbb2F" //sepolia
+    const contractAddress = donatationTracker_contractAddresses.sepolia
     const donationContract = new ethers.Contract(
       contractAddress,
       donationTrackerAbi,
@@ -443,7 +444,7 @@ export const RegisterOrganizationPopup = ({
     const signer = provider.getSigner()
     console.log("provider", provider)
     console.log("signer", signer)
-    const contractAddress = "0xE8e1D0EC12BDc3eaA3510d898640e13ca48cbb2F" //sepolia
+    const contractAddress = "0xE35DE3EF40C8E86F1B9D467D76C6DC3408aBCDD0" //sepolia
     const donationContract = new ethers.Contract(
       contractAddress,
       donationTrackerAbi,
