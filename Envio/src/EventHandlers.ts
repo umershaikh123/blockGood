1a4941ccd7656569b2b34156619175365b4acb7b
umershaikh123 <umershaikh217@gmail.com>
2024-09-03 01:49:37 +0500

    .

--- Envio/src/EventHandlers.ts
+++ Envio/src/EventHandlers.ts
@@ -1,6 +1,7 @@
 /*
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
+
 import {
   Donationtracker,
   Donationtracker_CampaignCreated,
@@ -8,63 +9,59 @@ import {
   Donationtracker_DonationReceived,
   Donationtracker_ProofUploaded,
   Donationtracker_WithdrawalMade,
-} from "generated";
+  //@ts-ignore
+} from "generated"
 
-Donationtracker.CampaignCreated.handler(async ({ event, context }) => {
+Donationtracker.CampaignCreated.handler(async ({ event, context }: any) => {
   const entity: Donationtracker_CampaignCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     creator: event.params.creator,
-  };
+  }
 
-  context.Donationtracker_CampaignCreated.set(entity);
-});
+  context.Donationtracker_CampaignCreated.set(entity)
+})
 
-
-Donationtracker.CampaignEnded.handler(async ({ event, context }) => {
+Donationtracker.CampaignEnded.handler(async ({ event, context }: any) => {
   const entity: Donationtracker_CampaignEnded = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     creator: event.params.creator,
     refundedFee: event.params.refundedFee,
-  };
+  }
 
-  context.Donationtracker_CampaignEnded.set(entity);
-});
+  context.Donationtracker_CampaignEnded.set(entity)
+})
 
-
-Donationtracker.DonationReceived.handler(async ({ event, context }) => {
+Donationtracker.DonationReceived.handler(async ({ event, context }: any) => {
   const entity: Donationtracker_DonationReceived = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     donor: event.params.donor,
     amount: event.params.amount,
-  };
+  }
 
-  context.Donationtracker_DonationReceived.set(entity);
-});
+  context.Donationtracker_DonationReceived.set(entity)
+})
 
-
-Donationtracker.ProofUploaded.handler(async ({ event, context }) => {
+Donationtracker.ProofUploaded.handler(async ({ event, context }: any) => {
   const entity: Donationtracker_ProofUploaded = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     withdrawalId: event.params.withdrawalId,
     proofUrl: event.params.proofUrl,
-  };
+  }
 
-  context.Donationtracker_ProofUploaded.set(entity);
-});
+  context.Donationtracker_ProofUploaded.set(entity)
+})
 
-
-Donationtracker.WithdrawalMade.handler(async ({ event, context }) => {
+Donationtracker.WithdrawalMade.handler(async ({ event, context }: any) => {
   const entity: Donationtracker_WithdrawalMade = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     admin: event.params.admin,
     amount: event.params.amount,
-  };
+  }
 
-  context.Donationtracker_WithdrawalMade.set(entity);
-});
-
+  context.Donationtracker_WithdrawalMade.set(entity)
+})
