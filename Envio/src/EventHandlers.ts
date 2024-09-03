175a9be9e14c550fa18552961cc0b444c48948b5
umershaikh123 <umershaikh217@gmail.com>
2024-09-03 20:01:37 +0500

    envio config update

--- Envio/src/EventHandlers.ts
+++ Envio/src/EventHandlers.ts
@@ -1,67 +1,70 @@
 /*
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
-
 import {
-  Donationtracker,
-  Donationtracker_CampaignCreated,
-  Donationtracker_CampaignEnded,
-  Donationtracker_DonationReceived,
-  Donationtracker_ProofUploaded,
-  Donationtracker_WithdrawalMade,
-  //@ts-ignore
-} from "generated"
+  DonationTracker,
+  DonationTracker_CampaignCreated,
+  DonationTracker_CampaignEnded,
+  DonationTracker_DonationReceived,
+  DonationTracker_ProofUploaded,
+  DonationTracker_WithdrawalMade,
+} from "generated";
 
-Donationtracker.CampaignCreated.handler(async ({ event, context }: any) => {
-  const entity: Donationtracker_CampaignCreated = {
+DonationTracker.CampaignCreated.handler(async ({ event, context }) => {
+  const entity: DonationTracker_CampaignCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     creator: event.params.creator,
-  }
+  };
 
-  context.Donationtracker_CampaignCreated.set(entity)
-})
+  context.DonationTracker_CampaignCreated.set(entity);
+});
 
-Donationtracker.CampaignEnded.handler(async ({ event, context }: any) => {
-  const entity: Donationtracker_CampaignEnded = {
+
+DonationTracker.CampaignEnded.handler(async ({ event, context }) => {
+  const entity: DonationTracker_CampaignEnded = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     creator: event.params.creator,
     refundedFee: event.params.refundedFee,
-  }
+  };
 
-  context.Donationtracker_CampaignEnded.set(entity)
-})
+  context.DonationTracker_CampaignEnded.set(entity);
+});
 
-Donationtracker.DonationReceived.handler(async ({ event, context }: any) => {
-  const entity: Donationtracker_DonationReceived = {
+
+DonationTracker.DonationReceived.handler(async ({ event, context }) => {
+  const entity: DonationTracker_DonationReceived = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     donor: event.params.donor,
     amount: event.params.amount,
-  }
+  };
 
-  context.Donationtracker_DonationReceived.set(entity)
-})
+  context.DonationTracker_DonationReceived.set(entity);
+});
 
-Donationtracker.ProofUploaded.handler(async ({ event, context }: any) => {
-  const entity: Donationtracker_ProofUploaded = {
+
+DonationTracker.ProofUploaded.handler(async ({ event, context }) => {
+  const entity: DonationTracker_ProofUploaded = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     withdrawalId: event.params.withdrawalId,
     proofUrl: event.params.proofUrl,
-  }
+  };
 
-  context.Donationtracker_ProofUploaded.set(entity)
-})
+  context.DonationTracker_ProofUploaded.set(entity);
+});
 
-Donationtracker.WithdrawalMade.handler(async ({ event, context }: any) => {
-  const entity: Donationtracker_WithdrawalMade = {
+
+DonationTracker.WithdrawalMade.handler(async ({ event, context }) => {
+  const entity: DonationTracker_WithdrawalMade = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     campaignId: event.params.campaignId,
     admin: event.params.admin,
     amount: event.params.amount,
-  }
+  };
 
-  context.Donationtracker_WithdrawalMade.set(entity)
-})
+  context.DonationTracker_WithdrawalMade.set(entity);
+});
+
--- Envio/test/Test.ts
+++ Envio/test/Test.ts
@@ -1,36 +1,36 @@
-// import assert from "assert";
-// import {
-//   TestHelpers,
-//   Donationtracker_CampaignCreated
-// } from "generated";
-// const { MockDb, Donationtracker } = TestHelpers;
+import assert from "assert";
+import { 
+  TestHelpers,
+  DonationTracker_CampaignCreated
+} from "generated";
+const { MockDb, DonationTracker } = TestHelpers;
 
-// describe("Donationtracker contract CampaignCreated event tests", () => {
-//   // Create mock db
-//   const mockDb = MockDb.createMockDb();
+describe("DonationTracker contract CampaignCreated event tests", () => {
+  // Create mock db
+  const mockDb = MockDb.createMockDb();
 
-//   // Creating mock for Donationtracker contract CampaignCreated event
-//   const event = Donationtracker.CampaignCreated.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+  // Creating mock for DonationTracker contract CampaignCreated event
+  const event = DonationTracker.CampaignCreated.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-//   it("Donationtracker_CampaignCreated is created correctly", async () => {
-//     // Processing the event
-//     const mockDbUpdated = await Donationtracker.CampaignCreated.processEvent({
-//       event,
-//       mockDb,
-//     });
+  it("DonationTracker_CampaignCreated is created correctly", async () => {
+    // Processing the event
+    const mockDbUpdated = await DonationTracker.CampaignCreated.processEvent({
+      event,
+      mockDb,
+    });
 
-//     // Getting the actual entity from the mock database
-//     let actualDonationtrackerCampaignCreated = mockDbUpdated.entities.Donationtracker_CampaignCreated.get(
-//       `${event.chainId}_${event.block.number}_${event.logIndex}`
-//     );
+    // Getting the actual entity from the mock database
+    let actualDonationTrackerCampaignCreated = mockDbUpdated.entities.DonationTracker_CampaignCreated.get(
+      `${event.chainId}_${event.block.number}_${event.logIndex}`
+    );
 
-//     // Creating the expected entity
-//     const expectedDonationtrackerCampaignCreated: Donationtracker_CampaignCreated = {
-//       id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-//       campaignId: event.params.campaignId,
-//       creator: event.params.creator,
-//     };
-//     // Asserting that the entity in the mock database is the same as the expected entity
-//     assert.deepEqual(actualDonationtrackerCampaignCreated, expectedDonationtrackerCampaignCreated, "Actual DonationtrackerCampaignCreated should be the same as the expectedDonationtrackerCampaignCreated");
-//   });
-// });
+    // Creating the expected entity
+    const expectedDonationTrackerCampaignCreated: DonationTracker_CampaignCreated = {
+      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+      campaignId: event.params.campaignId,
+      creator: event.params.creator,
+    };
+    // Asserting that the entity in the mock database is the same as the expected entity
+    assert.deepEqual(actualDonationTrackerCampaignCreated, expectedDonationTrackerCampaignCreated, "Actual DonationTrackerCampaignCreated should be the same as the expectedDonationTrackerCampaignCreated");
+  });
+});
